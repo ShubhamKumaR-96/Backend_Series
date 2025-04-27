0631452a1b0de198451ea701d8263e1ccb39344e
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-04-27 13:36:32 +0530

    added apierror,apiresponse and asynchandler

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@
 import dotenv from 'dotenv'
 
 import connectDB from './db/index.js'
+import { app } from './app.js'
  
 dotenv.config()
 
-connectDB()
+connectDB().then(()=>{
+    app.on("err",(err)=>{
+        console.log("Error: ",err)
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server running at http://localhost:${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("Error while connecting to mongo db ",err)
+})
